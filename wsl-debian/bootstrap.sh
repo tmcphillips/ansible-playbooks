838533713201447155d76eca3042efac5bc7a36c
Timothy McPhillips <tmcphillips@absoluteflow.org>
2019-03-02 18:54:20 -0800

    store ansible-venv inside clone of ansible-playbook repo

--- wsl-debian/bootstrap.sh
+++ wsl-debian/bootstrap.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-# Invoke this script in new WSL Debian environment:
+# Download and invoke this script in a new WSL Debian environment:
+#
 #   wget --no-check-certificate https://raw.githubusercontent.com/tmcphillips/ansible-playbooks/master/wsl-debian/bootstrap.sh -O bootstrap.sh
 #   bash bootstrap.sh
 
 function bootstrap_ansible_in_wsl_debian {
 
+    # request user password for invoking commands using sudo
     read -s -p "Enter your WSL Debian account password for using sudo: " password
 
-     # install git and OS-level dependencies of ansible as root
+    # install git and OS-level dependencies of ansible
     echo $password | sudo -S apt update
     echo $password | sudo -S apt-get -y install git python-pip python-dev libffi-dev libssl-dev
 
-    # install ansible in a Python virtual environment
+    # install virtualenv in the site-wide Python 2 environment
     echo $password | sudo -S pip install virtualenv
-    virtualenv ansible-venv --system-site-packages
-    . ansible-venv/bin/activate
-    pip install ansible
 
     # clone the ansible-playbooks repo
     git clone https://github.com/tmcphillips/ansible-playbooks.git
+
+    # install ansible in a new Python 2 virtual environment inside the local ansible-playbooks clone
+    virtualenv ansible-playbooks/.ansible-venv --system-site-packages
+    source ansible-playbooks/.ansible-venv/bin/activate
+    pip install ansible
 }
 
 bootstrap_ansible_in_wsl_debian
