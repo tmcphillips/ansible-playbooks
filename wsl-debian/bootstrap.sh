71fda4351035e92539e7c4997489cfd92772a9eb
Timothy McPhillips <tmcphillips@absoluteflow.org>
2019-03-02 21:39:36 -0800

    clone ansible-playbooks repo outside WSL filesystem if not already present

--- wsl-debian/bootstrap.sh
+++ wsl-debian/bootstrap.sh
@@ -17,13 +17,17 @@ function bootstrap_ansible_in_wsl_debian {
     # install virtualenv in the site-wide Python 2 environment
     echo $password | sudo -S pip install virtualenv
 
-    # clone the ansible-playbooks repo
-    git clone https://github.com/tmcphillips/ansible-playbooks.git
-
-    # install ansible in a new Python 2 virtual environment inside the local ansible-playbooks clone
-    virtualenv ansible-playbooks/.ansible-venv --system-site-packages
-    source ansible-playbooks/.ansible-venv/bin/activate
+    # install ansible in a new Python 2 virtual environment
+    virtualenv .venv-ansible-playbooks --system-site-packages
+    source .venv-ansible-playbooks/bin/activate
     pip install ansible
+
+    # clone the ansible-playbooks repo in the Windows user home directory tree if needed
+    if [ ! -d "/mnt/c/Users/tmcphill/GitRepos/ansible-playbooks" ]; then
+        mkdir -p /mnt/c/Users/tmcphill/GitRepos/
+        cd /mnt/c/Users/tmcphill/GitRepos/
+        git clone https://github.com/tmcphillips/ansible-playbooks.git
+    fi
 }
 
 bootstrap_ansible_in_wsl_debian
