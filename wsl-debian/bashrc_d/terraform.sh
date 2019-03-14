88a044cefcfeb0294afd708bb222c5c94207cfed
Timothy McPhillips <tmcphillips@absoluteflow.com>
2019-03-13 20:33:38 -0700

    add variable defining .ssh directory location

---
+++ wsl-debian/bashrc_d/terraform.sh
@@ -0,0 +1,3 @@
+export TF_VAR_ssh_key_directory=~/.ssh
+export TF_VAR_public_key_path=${TF_VAR_ssh_key_directory}/aws_instance_id_rsa.pub
+export TF_VAR_private_key_path=${TF_VAR_ssh_key_directory}/aws_instance_id_rsa
