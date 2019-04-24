7eedbe271355933ba346a41f77de7f260b00dced
Timothy McPhillips <tmcphillips@absoluteflow.org>
2019-04-23 17:22:35 -0700

    added instructions

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -1,8 +1,11 @@
 
-# Download and run this script in a PowerShell instance running as Administrator:
-#
-#   Invoke-WebRequest https://raw.githubusercontent.com/tmcphillips/ansible-playbooks/master/windows/bootstrap.ps1 -OutFile bootstrap.ps1
-#   PowerShell.exe -ExecutionPolicy RemoteSigned -File bootstrap.ps1
+# To run this script:
+# 1. Start a PowerShell instance running as Administrator.
+# 2. Change directory to temporary location such as C:\Temp
+# 3. Download this script:
+# --->  Invoke-WebRequest https://raw.githubusercontent.com/tmcphillips/ansible-playbooks/master/windows/bootstrap.ps1 -OutFile bootstrap.ps1
+# 4. Run this script script:
+# --->  PowerShell.exe -ExecutionPolicy RemoteSigned -File bootstrap.ps1
 
 # permanently enable PowerShell script execution
 Write-Output "BOOTSTRAP | Enable execution of locally authored and signed remote PowerShell scripts...";
