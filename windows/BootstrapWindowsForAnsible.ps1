9b43caf64393e3ad22d5febbd45101c469db25ed
Timothy McPhillips <tmcphillips@absoluteflow.org>
2019-04-23 23:24:18 -0700

    simplified bootstrap script

--- windows/BootstrapWindowsForAnsible.ps1
+++ windows/BootstrapWindowsForAnsible.ps1
@@ -4,74 +4,86 @@
 #
 # 1. Start a PowerShell instance running as Administrator.
 #
-# 2. Change directory to a temporary location such as C:\Temp
+# 2. Change directory to a temporary location such as C:\Temp.
 #
-#       cd C:\Temp
+#    cd C:\Temp
 #
 # 3. Download the script directly from GitHub:
 #
-#       Invoke-WebRequest https://raw.githubusercontent.com/tmcphillips/ansible-playbooks/master/windows/BootstrapWindowsForAnsible.ps1 -OutFile BootstrapWindowsForAnsible.ps1
+#    Invoke-WebRequest https://raw.githubusercontent.com/tmcphillips/ansible-playbooks/master/windows/BootstrapWindowsForAnsible.ps1 -OutFile BootstrapWindowsForAnsible.ps1
 #
-# 4. Execute the script:
+# 4. Execute the script.
 #
-#       PowerShell.exe -ExecutionPolicy RemoteSigned -File BootstrapWindowsForAnsible.ps1
+#    PowerShell.exe -ExecutionPolicy RemoteSigned -File BootstrapWindowsForAnsible.ps1
+#
+# 5. When prompted enter your Windows user name and password.
+#
+# 6. When prompted optionally enter the directory containing SSH config and key files to be copied to your .ssh directory.
 #
 #################################################################################################################################################################################
 
-Write-Host "BOOTSTRAP --> Enable execution of locally authored and signed remote PowerShell scripts...";
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine # -Confirm
-Write-Host "BOOTSTRAP --> Display PowerShell execution policy...";
+function Out-Log {
+
+    [cmdletbinding()]
+    Param(
+        [string]$message
+    )
+    Write-Host "BOOTSTRAP --> $message"
+}
+
+Out-Log "Enable execution of locally authored and signed remote PowerShell scripts..."
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+Out-Log "Display PowerShell execution policy..."
 get-executionpolicy -List
 
-Write-Host ""
-Write-Host "BOOTSTRAP --> Download and execute script for enabling remote system management by Ansible using WinRM...";
-Invoke-WebRequest https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -Outfile ConfigureRemotingForAnsible.ps1
-PowerShell.exe -File ConfigureRemotingForAnsible.ps1
+Out-Log "Download and execute script for enabling remote system management by Ansible using WinRM...";
+$WinRMEnableScript = "ConfigureRemotingForAnsible.ps1"
+Invoke-WebRequest https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/$WinRMEnableScript  -Outfile $WinRMEnableScript
+PowerShell.exe -File $WinRMEnableScript
 
-Write-Host "BOOTSTRAP --> Enable Windows Subsystem for Linux...";
+Out-Log "Enable Windows Subsystem for Linux..."
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 if ($null -eq (Get-Command "choco.exe" -ErrorAction SilentlyContinue)) {
-    Write-Host "BOOTSTRAP --> Install Chocolatey...";
+    Out-Log "Install Chocolatey..."
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 } else {
-    Write-Host "BOOTSTRAP --> Chocolatey already installed...";
+    Out-Log "Chocolatey already installed..."
 }
 
 if ($null -eq (Get-Command "git.exe" -ErrorAction SilentlyContinue)) {
-    Write-Host "BOOTSTRAP --> Install Git...";
+    Out-Log "Install Git..."
     choco upgrade git
 } else {
-    Write-Host "BOOTSTRAP --> Git already installed...";
+    Out-Log "Git already installed...";
 }
 
-Write-Host "BOOTSTRAP --> Get the user's credentials";
+Out-Log "Get the user's credentials"
 $UserCredential = Get-Credential -Message "Enter name and password for your Windows account"
-
 
 $SshKeySourceDir = Read-Host "Enter directory containing files to copy to .ssh directory"
 
-Write-Host "BOOTSTRAP --> Run the user account bootstrap script as the user...";
+Out-Log "Run the user account bootstrap script as the user..."
 $Job = Start-Job -Credential $UserCredential {
 
     Write-Host "BOOTSTRAP --> Work in user's home directory...";
     Set-Location $Env:HOMEPATH
 
     $UserSshDir = "$Env:HOMEPATH\.ssh"
-
     if (-Not (Test-Path -Path $UserSshDir)) {
-        Write-Host "BOOTSTRAP --> Create the user's .ssh directory...";
+        Write-Host "BOOTSTRAP --> Create the user's .ssh directory..."
         New-Item -ItemType directory -Path $UserSshDir
     }
 
     if ($using:SshKeySourceDir) {
-        Write-Host "BOOTSTRAP --> Copy keys to the user's .ssh directory...";
+        Write-Host "BOOTSTRAP --> Copy keys to the user's .ssh directory..."
         Copy-Item "$using:SshKeySourceDir\*" -Destination $UserSshDir
     }
 
-    if (-Not (Test-Path -Path .\GitRepos)) {
-        Write-Host "BOOTSTRAP --> Create the GitRepos directory...";
-        New-Item -ItemType directory -Path GitRepos
+    $GitReposDir = "$Env:HOMEPATH\GitRepos"
+    if (-Not (Test-Path -Path $GitReposDir)) {
+        Write-Host "BOOTSTRAP --> Create the GitRepos directory..."
+        New-Item -ItemType directory -Path $GitReposDir
     }
 }
 
