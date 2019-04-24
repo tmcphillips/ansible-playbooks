78c49af91466ae291052b42fcb383fae07368b6e
Timothy McPhillips <tmcphillips@absoluteflow.org>
2019-04-23 17:31:19 -0700

    improved instructions

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -1,16 +1,25 @@
 
-# To run this script:
+################################################################################################################################################
+# How to run this script on a new installation of Windows:
+#
 # 1. Start a PowerShell instance running as Administrator.
-# 2. Change directory to temporary location such as C:\Temp
-# 3. Download this script:
-# --->  Invoke-WebRequest https://raw.githubusercontent.com/tmcphillips/ansible-playbooks/master/windows/bootstrap.ps1 -OutFile bootstrap.ps1
-# 4. Run this script script:
-# --->  PowerShell.exe -ExecutionPolicy RemoteSigned -File bootstrap.ps1
+#
+# 2. Change directory to a temporary location such as C:\Temp
+#
+#       cd C:\Temp
+#
+# 3. Download the script directly from GitHub:
+#
+#       Invoke-WebRequest https://raw.githubusercontent.com/tmcphillips/ansible-playbooks/master/windows/bootstrap.ps1 -OutFile bootstrap.ps1
+#
+# 4. Execute the script:
+#
+#       PowerShell.exe -ExecutionPolicy RemoteSigned -File bootstrap.ps1
+################################################################################################################################################
 
 # permanently enable PowerShell script execution
 Write-Output "BOOTSTRAP | Enable execution of locally authored and signed remote PowerShell scripts...";
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine # -Confirm
-
 Write-Output "BOOTSTRAP | Display PowerShell execution policy...";
 get-executionpolicy -List
 
